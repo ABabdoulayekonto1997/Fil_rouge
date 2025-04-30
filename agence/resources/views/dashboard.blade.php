909987bc09a06329360be70b6a3378d5a79d254c
abddoulaye konto <mahamatkonto24@gmail.com>
2025-04-30 16:47:42 +0200

    liaison entre le accueil et le dasbord

--- agence/resources/views/dashboard.blade.php
+++ agence/resources/views/dashboard.blade.php
@@ -8,12 +8,15 @@
     <div class="bg-[#0C4069] h-full w-[188px]">
         <ul class="text-white p-2 space-y-2">
             <!-- Accueil -->
+            <a href="{{ url('/') }}">
+
             <li class="flex items-center p-2 rounded hover:bg-white hover:bg-opacity-20 hover:text-white transition duration-200 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                 </svg>
                 Accueil
             </li>
+            </a>
 
             <!-- Réservation -->
             <li class="flex items-center p-2 rounded hover:bg-white hover:bg-opacity-20 hover:text-white transition duration-200 cursor-pointer">
@@ -40,13 +43,14 @@
             </li>
 
             <!-- Profil -->
+            <a href="{{ route('profile.edit') }}">
             <li class="flex items-center p-2 rounded hover:bg-white hover:bg-opacity-20 hover:text-white transition duration-200 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
                 Profil
             </li>
-
+            </a>
             <!-- Déconnexion -->
             <li class="flex items-center p-2 rounded hover:bg-white hover:bg-opacity-20 hover:text-white transition duration-200">
                 <form action="{{ route('logout') }}" method="post" class="w-full">
