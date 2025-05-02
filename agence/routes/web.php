a6ae47a51f74bc4a081bcb69ed363ed93f55b8f6
abddoulaye konto <mahamatkonto24@gmail.com>
2025-05-02 18:28:28 +0200

    ouff

--- agence/routes/web.php
+++ agence/routes/web.php
@@ -27,8 +27,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    // Add this route with your other routes
-    Route::put('/user/update', [UserController::class, 'update'])->name('user.update');
 });
 
 require __DIR__.'/auth.php';
